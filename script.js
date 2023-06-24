403a56b4734b4da846739eb4171ec359d97e3b72
Gaurav Soni <120136122+gaurav8158@users.noreply.github.com>
2023-06-25 01:07:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function updateTime() {
+	const now = new Date();
+	const sec = now.getSeconds();
+	const min = now.getMinutes();
+	const hr = now.getHours();
+    const dt = now.getDate();
+	const mo = now.getMonth();
+	const yr = now.getFullYear();
+
+	document.getElementById("timer").innerText=`${dt}/${mo}/${yr}, ${hr}:${min}:${sec}`;
+}
+
+setInterval(updateTime, 500);
